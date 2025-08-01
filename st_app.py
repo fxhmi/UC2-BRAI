467f19cd2a2a994bcc11a84e48b7d2f0afdbcc84
fxhmi <fahmitaib1216@gmail.com>
2025-08-01 08:32:04 +0800

    MY Time

--- st_app.py
+++ st_app.py
@@ -583,20 +583,21 @@ js_code = """
 """
 
 client_time_malaysia = st_javascript(js_code, key="get_malaysia_time")
-today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%d %H:%M:%S")
+today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%dT%H:%M:%S")
 
 client_time_malaysia = client_time_malaysia.strip()  # remove extra spaces
 
 try:
     today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%dT%H:%M:%S")
+
 except ValueError:
     # try alternate format with space instead of T
-    today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%d %H:%M:%S")
+    today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%dT%H:%M:%S")
+
 
 
 if client_time_malaysia is not None:
-    today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%d %H:%M:%S")
-
+    today = datetime.datetime.strptime(client_time_malaysia, "%Y-%m-%dT%H:%M:%S")
 
     disrupted_day_of_week = today.weekday()
     disrupted_month = today.month
